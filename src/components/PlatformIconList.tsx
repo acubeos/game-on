25c22770552cd26710526c1d00511aae8bf2bcb6
acubeos <80309463+acubeos@users.noreply.github.com>
2023-06-04 13:46:49 +0100

    Displaying Platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import { Icon } from "@chakra-ui/react"
+import { Platform } from "../hooks/UseGames"
+import { HStack } from "@chakra-ui/react"
+
+import {
+	FaWindows,
+	FaPlaystation,
+	FaXbox,
+	FaApple,
+	FaLinux,
+	FaAndroid,
+} from "react-icons/fa"
+import { MdPhoneIphone } from "react-icons/md"
+import { SiNintendo } from "react-icons/si"
+import { BsGlobe } from "react-icons/bs"
+import { IconType } from "react-icons"
+
+interface Props {
+	platforms: Platform[]
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+	const iconMap: { [key: string]: IconType } = {
+		pc: FaWindows,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		nintendo: SiNintendo,
+		mac: FaApple,
+		linux: FaLinux,
+		ios: MdPhoneIphone,
+		web: BsGlobe,
+		android: FaAndroid,
+	}
+
+	return (
+		<HStack marginY={2}>
+			{platforms.map((platform) => (
+				<Icon as={iconMap[platform.slug]} color={"gray.500"} />
+			))}
+		</HStack>
+	)
+}
+
+export default PlatformIconList
