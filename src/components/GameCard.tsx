24e273fbba0c6e235619eabdd31de421b69c1747
acubeos <80309463+acubeos@users.noreply.github.com>
2023-06-04 12:03:36 +0100

    Building Game card

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Game } from "../hooks/UseGames"
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+
+interface Props {
+	game: Game
+}
+
+const GameCard = ({ game }: Props) => {
+	return (
+		<Card borderRadius={"10"} overflow={"hidden"}>
+			<Image src={game.background_image} />
+			<CardBody>
+				<Heading fontSize={"2xl"}>{game.name}</Heading>
+			</CardBody>
+		</Card>
+	)
+}
+
+export default GameCard
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,21 @@
-import { Text } from "@chakra-ui/react"
+import { SimpleGrid, Text } from "@chakra-ui/react"
 import UseGames from "../hooks/UseGames"
+import GameCard from "./GameCard"
 
 const GameGrid = () => {
 	const { error, games } = UseGames()
 	return (
 		<>
 			{error && <Text>{error}</Text>}
-			<ul>
+			<SimpleGrid
+				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+				padding={10}
+				spacing={10}
+			>
 				{games.map((game) => (
-					<li key={game.id}>{game.name}</li>
+					<GameCard key={game.id} game={game} />
 				))}
-			</ul>
+			</SimpleGrid>
 		</>
 	)
 }
