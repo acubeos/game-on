24d7b54947dc0ec307b6165c1f8a3acd2f5d7743
acubeos <80309463+acubeos@users.noreply.github.com>
2023-06-11 21:40:58 +0100

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
 
 const GameGrid = () => {
-	const { error, games, isLoading } = UseGames()
+	const { error, data, isLoading } = UseGames()
 	const skeletons = [1, 2, 3, 4, 5, 6]
 
 	return (
@@ -22,7 +22,7 @@ const GameGrid = () => {
 							<GameCardSkeleton key={"skeleton"} />
 						</GameCardContainer>
 					))}
-				{games.map((game) => (
+				{data.map((game) => (
 					<GameCardContainer>
 						<GameCard key={game.id} game={game} />
 					</GameCardContainer>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,10 @@
-import React from "react"
 import useGenres from "../hooks/UseGenres"
 
 const GenreList = () => {
-	const { genres } = useGenres()
+	const { data } = useGenres()
 	return (
 		<ul>
-			{genres.map((genre) => (
+			{data.map((genre) => (
 				<li key={genre.id}>{genre.name}</li>
 			))}
 		</ul>
