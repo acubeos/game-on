b7a1c27331a5acd2de423a71f5e20f526ea9ff4d
acubeos <80309463+acubeos@users.noreply.github.com>
2023-06-18 12:15:21 +0100

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp"
 import ColorModeSwitch from "./ColorModeSwitch"
 import SearchInput from "./SearchInput"
 
-function NavBar() {
+interface Props {
+	onSearch: (searchText: string) => void
+}
+
+function NavBar({ onSearch }: Props) {
 	return (
 		<HStack paddingRight={"20px"} pl={"10px"} pt={"10px"} pb={"10px"}>
 			<Image src={logo} boxSize={"60px"} />
-			<SearchInput />
+			<SearchInput onSearch={onSearch} />
 			<ColorModeSwitch />
 		</HStack>
 	)
--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -19,7 +19,9 @@ const UseGames = (gameQuery: GameQuery) => useData<Game>('/games',
 {params:{
 	genres: gameQuery.genre?.id, 
 	platforms: gameQuery.platform?.id, 
-	ordering: gameQuery.sortOrder}}, 
+	ordering: gameQuery.sortOrder,
+	search: gameQuery.searchText}
+}, 
 [gameQuery])
 
 export default UseGames
