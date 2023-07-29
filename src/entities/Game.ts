503682a605b804c7311454509084a2bb0e0173bd
acubeos <80309463+acubeos@users.noreply.github.com>
2023-07-29 23:32:40 +0100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./Genre"
 import { Platform } from "./Platform"
+import { Publisher } from "./Publishers"
 
 export interface Game {
 	id: number
 	slug: string
 	name: string
+	genres: Genre[]
+	publishers: Publisher[]
 	background_image: string
 	parent_platforms: { platform: Platform }[]
 	metacritic: number
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,8 +1,8 @@
-import React from "react"
 import { useParams } from "react-router-dom"
 import useGame from "../hooks/useGame"
-import { Heading, Spinner, Text } from "@chakra-ui/react"
+import { Heading, Spinner } from "@chakra-ui/react"
 import ExpandableText from "../components/ExpandableText"
+import GameAttributes from "../components/GameAttributes"
 
 const GameDetailsPage = () => {
 	const { slug } = useParams()
@@ -16,6 +16,7 @@ const GameDetailsPage = () => {
 		<>
 			<Heading>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
+			<GameAttributes game={game} />
 		</>
 	)
 }
