af240733cc7bfc91dc5565d7e6552ac1f71d4702
acubeos <80309463+acubeos@users.noreply.github.com>
2023-07-29 20:58:38 +0100

    Build expandable text

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useParams } from "react-router-dom"
 import useGame from "../hooks/useGame"
 import { Heading, Spinner, Text } from "@chakra-ui/react"
+import ExpandableText from "../components/ExpandableText"
 
 const GameDetailsPage = () => {
 	const { slug } = useParams()
@@ -14,7 +15,7 @@ const GameDetailsPage = () => {
 	return (
 		<>
 			<Heading>{game.name}</Heading>
-			<Text>{game.description_raw}</Text>
+			<ExpandableText>{game.description_raw}</ExpandableText>
 		</>
 	)
 }
