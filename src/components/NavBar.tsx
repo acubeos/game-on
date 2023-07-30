b419441d52df9f6707b9d0d98eb757b2d011d746
acubeos <80309463+acubeos@users.noreply.github.com>
2023-07-30 16:07:53 +0100

    Fix the nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,18 @@
-import { HStack, Image, Link } from "@chakra-ui/react"
+import { HStack, Image } from "@chakra-ui/react"
 import logo from "../assets/Logo.png"
 import ColorModeSwitch from "./ColorModeSwitch"
 import SearchInput from "./SearchInput"
+import { Link } from "react-router-dom"
 
 function NavBar() {
 	return (
 		<HStack padding='20px'>
-			<Link>
+			<Link to='/'>
 				<Image
 					borderRadius={10}
 					objectFit='contain'
 					src={logo}
-					boxSize={"60px"}
+					boxSize='50px'
 				/>
 			</Link>
 			<SearchInput />
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import useGame from "../hooks/useGame"
-import { Heading, Spinner } from "@chakra-ui/react"
+import { Heading, SimpleGrid, Spinner, Box } from "@chakra-ui/react"
 import ExpandableText from "../components/ExpandableText"
 import GameAttributes from "../components/GameAttributes"
 import GameScreenshots from "../components/GameScreenshots"
@@ -15,13 +15,17 @@ const GameDetailsPage = () => {
 	if (error || !game) throw error
 
 	return (
-		<>
-			<Heading>{game.name}</Heading>
-			<ExpandableText>{game.description_raw}</ExpandableText>
-			<GameAttributes game={game} />
-			<GameTrailer gameId={game.id} />
-			<GameScreenshots gameId={game.id} />
-		</>
+		<SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+			<Box>
+				<Heading>{game.name}</Heading>
+				<ExpandableText>{game.description_raw}</ExpandableText>
+				<GameAttributes game={game} />
+			</Box>
+			<Box>
+				<GameTrailer gameId={game.id} />
+				<GameScreenshots gameId={game.id} />
+			</Box>
+		</SimpleGrid>
 	)
 }
 
